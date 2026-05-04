7380a0c8c02be447b1cbd4f8a5dbf3eea38b0fc3
millen-morn23 <millenmorn23@gmail.com>
2026-05-04 10:07:06 +0300

    W01 Initial Site Creation complete

--- server.js
+++ server.js
@@ -2,15 +2,37 @@ import express from 'express';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
 // Define the port number
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// EJS setup
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Static files
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.render('home', { title: 'Home' });
 });
 
+app.get('/organizations', (req, res) => {
+  res.render('organizations', { title: 'Organizations' });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+app.get('/categories', (req, res) => {
+  res.render('categories', { title: 'Categories' });
+});
+
+// Server listener
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
