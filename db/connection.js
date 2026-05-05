3efb6be1621f88902939618cddc7a94849a9217a
millen-morn23 <millenmorn23@gmail.com>
2026-05-05 11:41:23 +0300

    Fix database connection for production

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,10 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'cse340',
-  password: 'milo1738',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
